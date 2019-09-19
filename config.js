03b2d3733afaee915cfa3bc8eed6b839422f74c1
jmais <36482623+jmais@users.noreply.github.com>
2019-09-18 21:23:25 -0400

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    db: {
-      uri: 'mongodb+srv://guest:1234@listingsjson-msfmj.mongodb.net/UF?retryWrites=true&w=majority', //place the URI of your mongo database here.
-    }
-  };
